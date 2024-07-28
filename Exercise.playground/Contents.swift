619949ac9872fe4969a1e503354c5857fcc3979e
ArwaMo <arwa-xx@hotmail.com>
2024-07-28 05:46:35 +0300

    write swift code

--- Exercise.playground/Contents.swift
+++ Exercise.playground/Contents.swift
@@ -1,3 +1,5 @@
-import UIKit
-
-var greeting = "Hello, playground"
+let name = "Arwa Alzahrani"
+var age = 26
+print("""
+Hi, \(name)! 🎉 \n Welcome to the programming community! You’re \(age) years old, and we look forward to seeing your creativity with us. Enjoy and learn a lot! 🚀
+""")
